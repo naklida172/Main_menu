8cce1bf0f5fe3be96c97ec174468896e2fe06158
Adi <Adi@LAPTOP-DNF9USU1>
2024-05-21 12:30:57 +0800

    Add project files.

---
+++ Main_menu/MainMenu.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main_menu
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+        class User
+        {
+            private string username;
+            private string email;
+            private string password;
+            private string role;
+            public User(string username, string email, string password, string role)
+            {
+                this.username = username;
+                this.email = email;
+                this.password = password;
+                this.role = role;
+            }
+
+            public bool Check_password(string password)
+            {
+                if(this.password==password)
+                    return true;
+                else return false;
+            }
+            public bool Check_login(string logindata)
+            {
+                if (logindata == this.email || logindata == this.username) return true;
+                else return false;
+            }
+            public string RolePlay()
+            { return this.role; }
+        }
+
+
+        List<User> Users = new List<User>
+        {
+            new User("john_doe", "john@example.com", "password123", "User"),
+            new User("guest", "guest@example.com", "guestpass", "Guest"),
+            new User("manager123", "manager@example.com", "securepwd", "Manager"),
+            new User("dev_guy", "dev@example.com", "devpassword", "Developer"),
+            new User("support_agent", "support@example.com", "helpme", "Support"),
+            new User("sales_rep", "sales@example.com", "sales123", "Sales"),
+            new User("marketing_guru", "marketing@example.com", "promo123", "Marketing"),
+            new User("cs_agent", "cs@example.com", "customerservice", "Customer Service"),
+            new User("testuser", "test@example.com", "test123", "Test")
+        };
+
+
+
+
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < Users.Count; i++)
+                {
+                    if (Users[i].Check_login(txtbxLogin.Text))
+                    {
+                        if (Users[i].Check_password(txtbxPassword.Text))
+                        {
+                            Manager obj1 = new Manager();
+                            obj1.ShowDialog();
+                        }
+                        else
+                        {
+                            lblTest.Text = "Wrong password";
+                        }
+                    }
+                }
+
+            } catch { lblTest.Text = "Error"; }
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTest_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Main_menu/Manager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main_menu
+{
+    public partial class Manager : Form
+    {
+        public void Hide_all_buttons()
+        {
+            btnMngMItms.Visible = false;
+            btnMngRsrvs.Visible = false;
+            btnViewRsrvRprt.Visible = false;
+            btnUpdtPrfl.Visible = false;
+            pictureBox1.Visible = false;
+        }
+        public void Unhide_all_buttons()
+        {
+            btnMngMItms.Visible = true;
+            btnMngRsrvs.Visible = true;
+            btnViewRsrvRprt.Visible = true;
+            btnUpdtPrfl.Visible = true;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+            pictureBox1.Visible = true;
+        }
+
+        public void Management_mode()
+        {
+            button1.Text = "Add";
+            button2.Text = "Edit";
+            button3.Text = "Delete";
+            Hide_all_buttons();
+            button1.Visible = true;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+        public void Reservation_view()
+        {
+            Hide_all_buttons();
+        }
+        class Menu_item
+        {
+            protected string name;
+            protected string category;
+            protected string description;
+            protected int price;
+        }
+
+        class Food_item : Menu_item
+        {
+            private string callories;
+            private string ingredients;
+            public Food_item(string name, string category, string description, int price,string callories, string ingredients)
+            {
+                this.name = name;
+                this.category = category;
+                this.description = description;
+                this.price = price;
+                this.callories = callories;
+                this.ingredients = ingredients;
+            }
+            public void Change_price(int price)
+            { this.price = price; }
+        }
+        public Manager()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnMngMItms_Click(object sender, EventArgs e)
+        {
+            Management_mode();
+        }
+
+        private void btnMngRsrvs_Click(object sender, EventArgs e)
+        {
+            Management_mode();
+        }
+
+        private void btnViewRsrvRprt_Click(object sender, EventArgs e)
+        {
+            Hide_all_buttons();
+        }
+
+        private void btnUpdtPrfl_Click(object sender, EventArgs e)
+        {
+            Hide_all_buttons();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Unhide_all_buttons();
+        }
+    }
+}
