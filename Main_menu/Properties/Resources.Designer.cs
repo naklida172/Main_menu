77375059461543f901c0bca0360799aaf6831811
Adi <homewer2016@gmail.com>
2024-06-09 17:26:57 +0800

    error fixed

--- Main_menu/Properties/Resources.Designer.cs
+++ Main_menu/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Main_menu.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap work_icon_11 {
-            get {
-                object obj = ResourceManager.GetObject("work-icon-11", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
